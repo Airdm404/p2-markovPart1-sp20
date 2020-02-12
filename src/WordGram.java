6d62b9ba8813e2a64da7029a023f242aaa91cee3
Edem Ahorlu <edem.ahorlu@duke.edu>
2020-02-12 09:42:48 -0500

    P2 completed

--- src/WordGram.java
+++ src/WordGram.java
@@ -1,9 +1,9 @@
-
+import java.util.*;
 /**
  * A WordGram represents a sequence of strings
  * just as a String represents a sequence of characters
  * 
- * @author YOUR NAME HERE
+ * @author Edem Ahorlu
  *
  */
 public class WordGram {
@@ -23,8 +23,12 @@ public class WordGram {
 		myWords = new String[size];
 		myToString = null;
 		myHash = 0;
+		for (int i = 0; i<size; i++) {
+			myWords[i] = source[start];
+			start++;
+		}
 
-		// TODO: initialize myWords
+
 	}
 
 	/**
@@ -41,51 +45,60 @@ public class WordGram {
 
 	/**
 	 * Complete this comment
-	 * @return
+	 * @return the number of words stored in instance variable myWords
 	 */
 	public int length(){
-		// TODO: change this
-		return 0;
+		return myWords.length;
 	}
 
 
 	/**
 	 * Complete appropriate comment here
-	 * @param o
-	 * @return
+	 * @param o	is checked to see if it's a WordGram object
+	 * @return false if o is not a WordGram, otherwise return true
 	 */
 	@Override
 	public boolean equals(Object o) {
-		if (! (o instanceof WordGram) || o == null){
+		if (!(o instanceof WordGram) || o == null) {
 			return false;
+		} else {
+			for (int g = 0; g < myWords.length; g++) {
+				if (!(myWords[g].equals(((WordGram) o).wordAt(g)))) {
+					return false;
+				}
+			}
+			return true;
 		}
-		// TODO: Complete this method
-
-		return true;
 	}
 
 	@Override
 	public int hashCode(){
 		// TODO: complete this method
+		myHash = this.toString().hashCode();
 		return myHash;
 	}
 	
 
 	/**
 	 * Create and complete this comment
-	 * @param last is last String of returned WordGram
-	 * @return
+	 * @param last is the string at the last index of the new WordGram
+	 * @return new WordGram (with k entries) whose first k-1 entries
+	 * are the same as the last k-1 entries of the original WordGram
 	 */
 	public WordGram shiftAdd(String last) {
-		WordGram wg = new WordGram(myWords,0,myWords.length);
+		String[] newword = new String[myWords.length];
+		System.arraycopy(myWords, 1,newword,0,myWords.length-1);
+		newword[newword.length-1] = last;
+		WordGram wg = new WordGram(newword,0,myWords.length);
 		// TODO: Complete this method
-
 		return wg;
 	}
 
 	@Override
 	public String toString(){
 		// TODO: Complete this method
+		List<String> wordlist = Arrays.asList( myWords);
+		myToString = String.join(" ", wordlist);
 		return myToString;
 	}
 }
